a3cd24c968128483c64654345a089a3b29b49c30
majetyr <sandy.maj24@mai.com>
2016-11-07 17:43:34 +0530

    First commit

---
+++ src/main/java/com/devops/education/App.java
@@ -0,0 +1,13 @@
+package com.devops.education;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       Util.printMessage("Hello World!");
+    }
+}
---
+++ src/main/java/com/devops/education/Util.java
@@ -0,0 +1,10 @@
+package com.devops.education;
+
+public class Util
+{
+  public static void printMessage(String message){
+            System.out.println(message);
+  }
+ 
+
+}
